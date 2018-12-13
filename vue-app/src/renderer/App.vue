40d6024bfc11dc9e681bf3bac927e19d08086bb6
Kyle Moses <kyle.moses@ascenaretail.com>
2018-12-13 15:28:44 -0500

    updates

--- vue-app/src/renderer/App.vue
+++ vue-app/src/renderer/App.vue
@@ -1,20 +1,41 @@
 <template>
-  <div id="app">
-    <landing-page></landing-page>
-  </div>
+		<landing-page></landing-page>
 </template>
 
 <script>
-  import LandingPage from '@/components/LandingPage';
+import LandingPage from "@/components/LandingPage";
 
-  export default {
-    name: 'vue-app',
-    components: {
-      LandingPage,
-    },
-  };
+export default {
+	name: "vue-app",
+	components: {
+		LandingPage
+	}
+};
 </script>
 
 <style>
-  /* CSS */
+* {
+	box-sizing: border-box;
+	margin: 0;
+	padding: 0;
+}
+
+body {
+	height: 100%;
+	width: 100%;
+	font-family: "Source Sans Pro", sans-serif;
+}
+body:after {
+	content: "";
+	opacity: 0.5;
+	background-image: url("./assets/hildebrantlarge.jpg");
+	background-size: cover;
+	background-position: 100% 100%;
+	top: 0;
+	left: 0;
+	bottom: 0;
+	right: 0;
+	position: fixed;
+	z-index: -1;
+}
 </style>
--- vue-app/src/renderer/components/LandingPage.vue
+++ vue-app/src/renderer/components/LandingPage.vue
@@ -31,30 +31,6 @@ export default {
 <style>
 @import url("https://fonts.googleapis.com/css?family=Source+Sans+Pro");
 
-* {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-}
-
-body {
-    height: 100%;
-    width: 100%;
-    font-family: "Source Sans Pro", sans-serif;
-}
-body:after {
-    content: "";
-    opacity: 0.5;
-    background-image: url('../assets/hildebrantlarge.jpg');
-    background-size: cover;
-    background-position: 100% 100%;
-    top: 0;
-    left: 0;
-    bottom: 0;
-    right: 0;
-    position: fixed;
-    z-index: -1;
-}
 #wrapper {
     height: auto;
     width: 90%;
